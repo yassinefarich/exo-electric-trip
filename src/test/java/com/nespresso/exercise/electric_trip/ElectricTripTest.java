1467d74e9383519523beff6716a2568658939e85
inverno <carlo.bottiglieri@gmail.com>
2015-08-26 14:15:11 +0200

    Added introduction comment

--- src/test/java/com/nespresso/exercise/electric_trip/ElectricTripTest.java
+++ src/test/java/com/nespresso/exercise/electric_trip/ElectricTripTest.java
@@ -5,6 +5,17 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * An electric car's endurance is given by the amount of energy it can store in its battery (KWh)
+ * and its efficiency (how many Km you can drive with one KWh of energy). Since electric
+ * chargers are not yet so frequent on the road it is important to plan a trip according to where
+ * the you can charge.
+ * The very first charging stations are found today on highways near cities.
+ * A trip is defined by a set of cities separated by the number of Km between two cities.
+ * It is useful to have system that tells you to stop when you are passing by a charger if there's
+ * no further charging infrastructure within the given range. This test suite will bring you to
+ * build such a system.
+ */
 public class ElectricTripTest {
 
     @Test
